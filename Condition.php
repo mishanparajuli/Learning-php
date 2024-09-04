1b94962d86375a56b88ab3c05d568e12020374e0
Mishan Parajuli <mparajuli1@stu.jsu.edu>
2024-09-05 01:18:25 +0545

    Create Condition.php

---
+++ Condition.php
@@ -0,0 +1,11 @@
+<?php
+$age = 20;
+
+if ($age < 18) {
+    echo "You are too young!";
+} elseif ($age >= 18 && $age <= 25) {
+    echo "You are a young adult!";
+} else {
+    echo "You are an adult!";
+}
+?>
